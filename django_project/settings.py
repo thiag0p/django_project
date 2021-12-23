51e605bf979dec7284c52b7cbc83955c9b3b9cff
thiago <thiagop.oceano@gmail.com>
2021-12-23 19:47:38 -0300

    Configurada SECRET_KEY nos ambientes do projeto.

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&8*6fs8vk&3jnia4i*-g_mrl*hltd&p@-^%6zowc3n1hs37xrl'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
