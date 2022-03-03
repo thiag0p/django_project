984bb82e611bf4466068c645fe281a288d0b50c9
thiago <thiagop.oceano@gmail.com>
2022-03-03 11:43:13 -0300

    debugado urls.py para ativação da debug_toolbar

--- django_project/urls.py
+++ django_project/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django_project import settings
+
 from django_project.base.views import home
 
 urlpatterns = [
